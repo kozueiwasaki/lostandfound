c9039d2f7ca33769c6ff7c28ce618b98334a3bcf
kocchan <59197715+kocchan@users.noreply.github.com>
2020-01-22 15:58:38 +0900

    changed files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "posts#index"
-  resources :posts
-  resources :users, only: :show
+  resources :posts do
+    resources :comments, only: :create
+  end
 end
